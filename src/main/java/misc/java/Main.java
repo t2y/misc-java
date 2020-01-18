c697fa5d7c24a79c0b4e260ad89a9e4ec6556cf9
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2020-01-18 10:51:03 +0900

    update

---
+++ src/main/java/misc/java/Main.java
@@ -0,0 +1,12 @@
+package misc.java;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+  public static void main(String[] args) {
+    log.info("start");
+    log.info("end");
+  }
+}
