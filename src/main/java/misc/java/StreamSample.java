952cd2c3e13b32b48127b96b910d29e3a4320efd
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2020-04-20 10:22:16 +0900

    update stream sample

--- src/main/java/misc/java/StreamSample.java
+++ src/main/java/misc/java/StreamSample.java
@@ -1,5 +1,7 @@
 package misc.java;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,12 @@ public class StreamSample {
   private static String[] ranges =
       IntStream.rangeClosed(0, 10).boxed().map(String::valueOf).toArray(String[]::new);
 
+  private static List<Tuple<Integer, String>> multiRanges =
+      IntStream.rangeClosed(0, 10)
+          .boxed()
+          .map(e -> Tuple.of(e, String.valueOf(e)))
+          .collect(Collectors.toList());
+
   public void show() {
     for (int i : IntStream.rangeClosed(0, 10).toArray()) {
       log.info("{}", i);
@@ -17,6 +25,10 @@ public class StreamSample {
     log.info("--------------------------------");
     for (String i : ranges) {
       log.info("{}", i);
+    }
+    log.info("--------------------------------");
+    for (Tuple<Integer, String> t : multiRanges) {
+      log.info("{}, {}", t.getLeft(), t.getRight());
     }
   }
 }
